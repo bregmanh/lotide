c99aec71aadc9c91b24a0bae55f1d72ccfabaea3
Hannah Bregman <bregmanhannah@gmail.com>
2020-05-26 20:13:31 +0000

    added assertEqualArray function

---
+++ assertArrayEqual.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } return true;
+  }
+}
+
+const assertArrayEqual = function (arr1, arr2) {
+ const result = eqArrays(arr1, arr2);
+  if (result) {
+    return console.log(`😀 Assertion Passed: "${arr1}" === "${arr2}"`);
+  } else {
+    return console.log(`🔥 Assertion Failed: "${arr1}" !== "${arr2}"`);
+
+  }
+};
+
+//TEST CODE
+assertArrayEqual([1, 2, 3], [1, 2, 3]); // => should PASS
