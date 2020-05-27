fe1fbb6589993b539368396d7a698abdc38a3079
Hannah Bregman <bregmanhannah@gmail.com>
2020-05-27 17:34:54 +0000

    function to count values in a list based on data provided in an object.

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    return console.log(`😀 Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    return console.log(`🔥 Assertion Failed: "${actual}" !== "${expected}"`);
+
+  }
+};
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+results={
+  
+};
+for(const item of allItems){
+  if(itemsToCount[item]){
+    if(results[item]){
+      results[item]+=1;
+    }else{
+      results[item]=1;
+    }
+  }
+  
+}
+
+console.log(results);
+return results;
+}
+//TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
