7765da04b919e1fbe62a34df2c898bf323729ba4
Hannah Bregman <bregmanhannah@gmail.com>
2020-05-28 20:02:00 +0000

    findKey function that finds the key based on the condition specified on the associated value

---
+++ findKey.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    return console.log(`😀 Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    return console.log(`🔥 Assertion Failed: "${actual}" !== "${expected}"`);
+
+  }
+};
+
+const findKey = function (obj, callback) {
+
+  for (let item in obj) {
+    if (callback(obj[item])) {
+      return item;
+    }
+  }
+}
+
+// TEST CODE
+
+let result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(result, "noma");
