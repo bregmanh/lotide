33c3c7c964fed412c0c9f6d46255c4a8daa44177
Hannah Bregman <bregmanhannah@gmail.com>
2020-05-28 18:55:02 +0000

    added map function to modify items in an array easily

---
+++ map.js
@@ -0,0 +1,40 @@
+
+
+const map = function(array, callback) {
+  const results=[];
+  for(let item of array){
+    results.push(callback(item))
+  }
+ 
+  return results;
+}
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } return true;
+  }
+}
+
+const assertArrayEqual = function (arr1, arr2) {
+ const result = eqArrays(arr1, arr2);
+  if (result) {
+    return console.log(`😀 Assertion Passed: "${arr1}" === "${arr2}"`);
+  } else {
+    return console.log(`🔥 Assertion Failed: "${arr1}" !== "${arr2}"`);
+
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word+" hi");
+
+//TEST CODE
+assertArrayEqual(results1, ['g','c','t','m','t' ]); // => should PASS
+assertArrayEqual(results2, ["ground hi", "control hi", "to hi", "major hi", "tom hi"]); // => should PASS
