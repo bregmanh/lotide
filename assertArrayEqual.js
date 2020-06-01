c55e535eccee640dd191f429baa3d4661cea245b
Hannah Bregman <bregmanhannah@gmail.com>
2020-06-01 17:23:30 +0000

    Refactored and modularazied assertArrayEqual and separated test code

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,18 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    } return true;
-  }
-}
+const eqArrays = require('./eqArrays');
+//Function that asserts - outputs the result of the test - to check if two arrays are equal
 
 const assertArrayEqual = function (arr1, arr2) {
- const result = eqArrays(arr1, arr2);
+  const result = eqArrays(arr1, arr2);
   if (result) {
     return console.log(`😀 Assertion Passed: "${arr1}" === "${arr2}"`);
   } else {
@@ -21,5 +11,4 @@ const assertArrayEqual = function (arr1, arr2) {
   }
 };
 
-//TEST CODE
-assertArrayEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+module.exports = assertArrayEqual;
